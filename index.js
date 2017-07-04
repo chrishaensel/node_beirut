ae725ecb50c0bca63cd8f72d27eb4a235bef4010
Christian Haensel <chris@chaensel.de>
2017-07-04 16:23:14 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var weather = require('weather-js');
+
+module.exports = {
+
+    /**
+     * get country data
+     */
+    getData: function () {
+        var data = require('./data.json') // data : [country : [cities]]
+        return data
+    },
+    /**
+     * Get the current weather information. Uses callback to pass data
+     * @param callback
+     */
+    getWeather : function(callback) {
+        weather.find({search: 'Beirut, Lebanon', degreeType: 'C'}, function(err, result) {
+            const wx = result[0].current
+            callback(wx)
+        });
+    }
+}
